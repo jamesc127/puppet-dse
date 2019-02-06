3325d28c6a9df2685db0b80b53db27ca9bd2f7bb
James Colvin <jamesc127@gmail.com>
2019-02-06 05:36:47 -0600

    updated hiera.yaml to version 5 https://puppet.com/docs/puppet/5.5/hiera_migrate.html

--- site.pp
+++ site.pp
@@ -1,10 +1,10 @@
+lookup('classes', {merge => unique}).include
 node default {
   # Define role base on hiera content
   # $role = hiera('role', 'bootstrap')
-  $role = lookup('role', 'bootstrap')
+  $role = lookup('role',undef,undef,'bootstrap')
   # $cluster = hiera('cluster', 'unassigned')
-  $cluster = lookup('cluster', 'unassigned')
+  $cluster = lookup('cluster',undef,undef,'unassigned')
   # Include classes defined in heira
-  hiera_include('classes')
-  # lookup('classes', {merge => unique}).include
+  # hiera_include('classes')
 }
